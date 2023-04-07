4d24c4ea3d80a31a5f9504d6ed6177bcf52f501f
Kent D <kent.daleng@nav.no>
2023-04-07 17:57:06 +0200

    update readme

--- dunder/README.md
+++ dunder/README.md
@@ -12,4 +12,79 @@ cn :)
 
 ## Description
 
-todo!
+A simple step sequencer and drum machine based on [EuclideanDrums](https://github.com/electro-smith/DaisyExamples/tree/master/pod/EuclideanDrums).
+The sequencer's length is set to 16 steps.
+
+## Controls
+
+The encoder button changes modes, and the current mode can be seen in LED 1.
+
+### Edit mode
+
+If LED 1 is red, we're in *edit mode*.
+
+| Control  | Description                                    |
+|----------|------------------------------------------------|
+| Encoder  | Moves the edit cursor along the step sequencer |
+| Button 1 | Places a kick trigger at the cursor            |
+| Button 2 | Places a snare trigger at the cursor           |
+
+
+### Play mode
+
+If LED 1 is blue, we're in *play mode*.
+In this mode, various attributes can be changed with the knobs.
+LED 2 indicates which attributes we're changing, or which "inner mode" we're in.
+
+| Control | Description            |
+|---------|------------------------|
+| Encoder | Changes the inner mode |
+
+#### Kick
+
+If LED 2 is off, we're in *kick mode*.
+
+| Control | Description                               |
+|---------|-------------------------------------------|
+| Knob 1  | Changes the kick volume envelope's decay  |
+| Knob 2  | Changes the kick volume envelope's attack |
+
+
+#### Snare
+
+If LED 2 is at 20% brightness, we're in *snare mode*.
+
+| Control | Description                         |
+|---------|-------------------------------------|
+| Knob 1  | Changes the snare envelope's decay  |
+| Knob 2  | Changes the snare envelope's attack |
+
+#### Reverb
+
+If LED 2 is at 40% brightness, we're in *reverb mode*.
+
+| Control | Description                 |
+|---------|-----------------------------|
+| Knob 1  | Changes the reverb feedback |
+| Knob 2  | Changes the reverb cutoff   |
+
+#### Overdrive
+
+If LED 2 is at 60% brightness, we're in *overdrive mode*.
+Although a reverb attribute is also modified here, I know it's a bit confusing.
+
+| Control | Description                      |
+|---------|----------------------------------|
+| Knob 1  | Changes the overdrive amount     |
+| Knob 2  | Changes the reverb wet/dryness   |
+
+
+#### Tempo
+
+If LED 2 is at 80% brightness, we're in *tempo mode*.
+
+| Control | Description       |
+|---------|-------------------|
+| Knob 1  | Changes the tempo |
+
+
--- dunder/dunder.cpp
+++ dunder/dunder.cpp
@@ -26,12 +26,12 @@ uint8_t cursor, playCursor = 0; // cursor for keeping track of positions in step
 
 float k1old, k2old = 0.f; // for comparing with old pot values
 
-uint8_t innerMode, outerMode = 0; // for keeping track of modes
-								  // - outerMode == 0 is "edit", == 1 is "play"
-								  //   "edit" lets us modify kickSeq and snareSeq with buttons
-								  //   "play" loops the sequences and lets us modify parameters based on innerMode
-								  // innerMode only matters during "play".
-								  // == 0 is "kick", == 1 is "snare", == 2 is "reverb", == 3 is "overdrive", == 4 is "tempo"?
+uint8_t outerMode = 0; // == 0 is "edit", == 1 is "play"
+					   //   "edit" lets us modify kickSeq and snareSeq with buttons
+					   //   "play" loops the sequences and lets us modify parameters based on innerMode
+uint8_t innerMode = 0 // innerMode only matters during "play".
+					  // == 0 is "kick", == 1 is "snare", == 2 is "reverb", == 3 is "overdrive", == 4 is "tempo"?
+                      // knobs will be available to adjust various attributes
 
 float tempo = 3;
 float drywet;
@@ -181,40 +181,27 @@ void UpdateEncoder()
 	if(outerMode == MODE_EDIT) {
 		cursor += hardware.encoder.Increment();
 		cursor = (cursor % MAX_LENGTH + MAX_LENGTH) % MAX_LENGTH;
-	}
-	if(outerMode == MODE_PLAY) {
+	} else {
 		innerMode += hardware.encoder.Increment();
 		innerMode = (innerMode % TOTAL_MODES + TOTAL_MODES) % TOTAL_MODES;
 		hardware.led2.Set(0, 0.2f*innerMode, 0);
 	}
 }
 
-bool hasChanged(float val, float oldVal) {
-	return abs(oldVal - val) > 0.01;
+bool HasChanged(float val, float oldVal) {
+	return abs(oldVal - val) > 0.01; // this delta value is difficult
 }
 
-void EditEnvelope(AdEnv& env, uint8_t type, float val) {
-	env.SetTime(type, val);
-}
-
-void EditDrive(float val) {
-	drive.SetDrive(val);
-}
-
-void EditReverbFeedback(float val) {
-	rev.SetFeedback(val);
-}
-
-void EditReverbCutoff(float val) {
+void SetReverbCutoff(float val) {
 	float cutoff_max = hardware.AudioSampleRate() / 2;
 	rev.SetLpFreq(val*cutoff_max);
 }
 
-void EditDrywet(float val) {
+void SetDrywet(float val) {
 	drywet = val;
 }
 
-void EditTempo(float val) {
+void SetTempo(float val) {
 	tempo = val*10.0;
 	tempo = std::fminf(tempo, 10.f);
     tempo = std::fmaxf(.5f, tempo);
@@ -228,24 +215,24 @@ void UpdateKnobs()
     float k2 = hardware.knob2.Process();
 	
 	if(outerMode == MODE_PLAY) {
-		if (hasChanged(k1, k1old)) {
+		if (HasChanged(k1, k1old)) {
 			switch (innerMode) {
-				case MODE_SNARE: EditEnvelope(snareEnv, ADENV_SEG_DECAY, k1*10.0); break;
-				case MODE_DRIVE: EditDrive(k1); break;
-				case MODE_REVERB: EditReverbFeedback(k1); break;
-				case MODE_TEMPO: EditTempo(k1);
-				default: EditEnvelope(kickVolEnv, ADENV_SEG_DECAY, k1*10.0); break;
+				case MODE_SNARE: snareEnv.SetTime(ADENV_SEG_DECAY, k1*10.0); break;
+				case MODE_DRIVE: drive.SetDrive(k1); break;
+				case MODE_REVERB: rev.SetFeedback(k1); break;
+				case MODE_TEMPO: SetTempo(k1);
+				default: kickVolEnv.SetTime(ADENV_SEG_DECAY, k1*10.0); break;
 			}
 		
 			k1old = k1;
 		}
-		if (hasChanged(k2, k2old)) {
+		if (HasChanged(k2, k2old)) {
 			switch (innerMode) {
-				case MODE_SNARE: EditEnvelope(snareEnv, ADENV_SEG_ATTACK, k2*10.0); break;
-				case MODE_DRIVE: EditDrywet(k2); break;
-				case MODE_REVERB: EditReverbCutoff(k2); break;
+				case MODE_SNARE: snareEnv.SetTime(ADENV_SEG_ATTACK, k2*10.0); break;
+				case MODE_DRIVE: SetDrywet(k2); break; // kind of a silly place to put it but alas
+				case MODE_REVERB: SetReverbCutoff(k2); break;
 				case MODE_TEMPO: break;
-				default: EditEnvelope(kickVolEnv, ADENV_SEG_ATTACK, k2*10.0); break;
+				default: kickVolEnv.SetTime(ADENV_SEG_ATTACK, k2*10.0); break;
 			}
 
 			k2old = k2;
@@ -261,9 +248,9 @@ void UpdateKnobs()
 
 void UpdateButtons() {
 	if(outerMode == MODE_EDIT) {
-		if (hardware.button2.RisingEdge()) {
+		if (hardware.button1.RisingEdge()) {
 			kickSeq[cursor] = !kickSeq[cursor];
-		} else if (hardware.button1.RisingEdge()) {
+		} else if (hardware.button2.RisingEdge()) {
 			snareSeq[cursor] = !snareSeq[cursor];
 		}
 	}
